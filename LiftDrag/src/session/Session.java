3a41b899762a92f226ae2643c99a0d0dd5c40089
pandachan <pandachan@linux-panda>
2015-03-05 18:46:17 +0000

    All links with database not tested 2

--- LiftDrag/src/session/Session.java
+++ LiftDrag/src/session/Session.java
@@ -15,6 +15,17 @@ import java.util.List;
  * @author madfist
  */
 public class Session implements SessionInfo, Logging {    
+    private String name;
+    private byte[] passwordHash;
+    private SessionControl control;
+    private int id;
+    private float minangle;
+    private float maxangle;
+    private float minthickness;
+    private float maxthickness;
+    private float mincamber;
+    private float maxcamber;
+    
     public Session(String n, String pw, SessionControl ctrl) {
         name = n;
         try {
@@ -24,6 +35,17 @@ public class Session implements SessionInfo, Logging {
         } catch (NoSuchAlgorithmException ex) {
             System.out.println("no md5");
         }
+        control = ctrl;
+    }
+    
+    public Session(SessionControl ctrl, int id, String username,float minangle, float maxangle, float minthickness, float maxthickness, float mincamber, float maxcamber){
+        this.id=id;
+        this.minangle=minangle;
+        this.maxangle=maxangle;
+        this.minthickness=minthickness;
+        this.maxthickness=maxthickness;
+        this.mincamber=mincamber;
+        this.maxcamber=maxcamber;
         control = ctrl;
     }
     
@@ -56,8 +78,34 @@ public class Session implements SessionInfo, Logging {
         //database call through control
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    public float getMinangle() {
+        return minangle;
+    }
+
+    public float getMaxangle() {
+        return maxangle;
+    }
+
+    public float getMinthickness() {
+        return minthickness;
+    }
+
+    public float getMaxthickness() {
+        return maxthickness;
+    }
+
+    public float getMincamber() {
+        return mincamber;
+    }
+
+    public float getMaxcamber() {
+        return maxcamber;
+    }
+
+    public int getId() {
+        return id;
+    }
     
-    private String name;
-    private byte[] passwordHash;
-    private SessionControl control;
+
 }
